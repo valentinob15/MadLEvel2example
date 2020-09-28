6b4d741c253c45730e6deba2b0919c6807b4bf1d
valentino <valentinob15@hotmail.com>
2020-09-28 18:42:58 +0200

    finished the example

--- app/src/main/java/com/example/madlevelexample2/MainActivity.kt
+++ app/src/main/java/com/example/madlevelexample2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevelexample2.databinding.ActivityMainBinding
@@ -13,8 +14,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val reminders = arrayListOf<Reminder>()
-    private val reminderAdapter =ReminderAdapter(reminders)
-    private lateinit var binding : ActivityMainBinding
+    private val reminderAdapter = ReminderAdapter(reminders)
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,25 +24,59 @@ class MainActivity : AppCompatActivity() {
         initViews()
     }
 
-    private fun initViews(){
-        binding.rvReminders.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+    private fun initViews() {
+        binding.rvReminders.layoutManager =
+            LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
         binding.rvReminders.adapter = reminderAdapter
-        binding.rvReminders.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+        binding.rvReminders.addItemDecoration(
+            DividerItemDecoration(
+                this@MainActivity,
+                DividerItemDecoration.VERTICAL
+            )
+        )
         binding.etReminder.text?.clear()
         binding.btnAddReminder.setOnClickListener {
             val reminder = binding.etReminder.text.toString();
+
             addReminder(reminder)
         }
+        createImageTouchHelper().attachToRecyclerView(rvReminders)
+
+
 
     }
 
-    private fun addReminder(reminder : String){
-        if (reminder.isNotBlank()){
+    private fun addReminder(reminder: String) {
+        if (reminder.isNotBlank()) {
             reminders.add(Reminder(reminder))
             reminderAdapter.notifyDataSetChanged()
-        }else{
-            Snackbar.make(etReminder, "You must fill in the input field!", Snackbar.LENGTH_SHORT).show()
+        } else {
+            Snackbar.make(etReminder, "You must fill in the input field!", Snackbar.LENGTH_SHORT)
+                .show()
         }
+    }
+
+    private fun createImageTouchHelper(): ItemTouchHelper {
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                reminders.removeAt(position)
+                reminderAdapter.notifyDataSetChanged()
+            }
+
+
         }
+        return ItemTouchHelper(callback)
 
     }
+}
